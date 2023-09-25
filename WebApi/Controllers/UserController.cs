84e6918865cc56db75e45ba42c1d05000851a5a7
TajievOlimjon <tajiev0711@gmail.com>
2023-09-25 14:20:15 +0500

    test

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -31,7 +31,7 @@ namespace WebApi
         }
 
         // GET: api/users/5
-        /*[HttpGet("{id}")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUser(int id)
         {
             var user = await _dbContext.Users.FindAsync(id);
@@ -42,20 +42,20 @@ namespace WebApi
             }
 
             return user;
-        }*/
+        }
 
         // POST: api/users
-       /* [HttpPost]
+        [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
             _dbContext.Users.Add(user);
             await _dbContext.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
-        }*/
+        }
 
         // PUT: api/users/5
-       /* [HttpPut("{id}")]
+        [HttpPut("{id}")]
         public async Task<IActionResult> PutUser(int id, User user)
         {
             if (id != user.Id)
@@ -82,7 +82,7 @@ namespace WebApi
             }
 
             return NoContent();
-        }*/
+        }
 
         // DELETE: api/users/5
         [HttpDelete("{id}")]
